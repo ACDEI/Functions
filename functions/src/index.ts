9c847f5b467f8a7710bc7ed06be139c8af1195bb
0619691092 <48439695+0619691092@users.noreply.github.com>
2020-11-10 10:24:04 +0100

    Update basic operations users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,30 +15,78 @@ const app = express();
 //   response.send("Hello from Firebase!");
 // });
 
+
+
+//GET
+//get usuarios
 app.get("/users/", async (req, res) => {
-    const snapshot = await admin.firestore().collection("users").get();
+    try {
 
-    console.log(snapshot);
+        const snapshot = await admin.firestore().collection("users").get();
+        const users:any = [];
+        snapshot.forEach((doc) => {
+            const id = doc.id;
+            const data = doc.data();
+            users.push({ id, ...data });
+        });
+        res.status(200).send(JSON.stringify(users));
 
-    const users:any = [];
-    snapshot.forEach((doc) => {
-        const id = doc.id;
-        const data = doc.data();
-
-        console.log(id, data);
-
-        users.push({ id, ...data });
-    });
-
-    res.status(200).send(JSON.stringify(users));
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
 });
+
+//get user id 
+app.get('/id/:id',async(req,res)=>{
+    try{
+        
+        const snapshot = await admin.firestore().collection('users').doc(req.params.id);
+        let product = await snapshot.get();
+        let user = product.data();
+        res.status(200).send(JSON.stringify(user));
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+//get user from ..to 
+
+
+
+//get count Users
+app.get("/countUsers", async(req,res)=> {
+    try{
+
+        const snapshot = await admin.firestore().collection("users").get();
+        res.status(200).send(JSON.stringify(snapshot.size));
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+
+
+
 
 app.post("/users/", async (req, res) => {
-    const user = req.body;
-  
-    await admin.firestore().collection("users").add(user);
-  
-    res.status(201).send();
+
+    try{
+
+        const user = req.body;
+        await admin.firestore().collection("users").add(user);
+        res.status(201).send();
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
 });
+
+
 
 exports.APIRest = functions.https.onRequest(app);
